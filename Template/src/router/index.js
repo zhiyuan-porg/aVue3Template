70ea900f53984e93a10dd19e582d093a0a19e6d7
zhiyuan-porg <3177294053@qq.com>
2022-03-04 14:27:41 +0800

    add flies

---
+++ Template/src/router/index.js
@@ -0,0 +1,12 @@
+//  import HelloWorld from '../views/HelloWorld.vue'
+import { createRouter,createWebHashHistory } from 'vue-router'
+const routes = [
+    {
+       
+    }
+]
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes
+  })
+export {router} 
